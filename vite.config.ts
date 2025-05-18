f60c751f9ecf2f3c4b5921b05922bf9289733d03
Madhusudhan K <madhusudhank963@gmail.com>
2025-05-18 12:35:00 +0530

    base url for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jsonViewer/', // <-- Add this line
-})
+  base: '/', // <-- For Vercel, use root!
+});
